264c9716403919f9982cd250ad1f5f594bb1e66f
Raymond Li <li.raymond@columbia.edu>
2020-06-18 13:24:25 -0400

    Prepare for demo

--- tools/validators/instance_validator/schema.py
+++ tools/validators/instance_validator/schema.py
@@ -1,16 +1,32 @@
 from strictyaml import Map, MapPattern, Str, Optional, YAMLValidationError, Any, load, Enum, Regex, Seq
 import sys
 
-# TODO make ontological structure error yaml examples
-# TODO add all states to translation_data_schema
-# TODO add manual check for translation_data_schema to de-duplicate units/unit_values/states
+
+'''
+demo:
+- good_building
+- bad_syntax
+- bad_tabbing
+- bad_dup_keys
+- good_connections
+- bad_connections
+- good_translation*
+- bad_translation*
+- good_link
+- bad_link
+
+questions:
+- for virtual entities, ID not required. so I think to check this in ontology, but where's a list of virtual types?
+- i'm not sure how to parse metadata?
+'''
 
 schema = MapPattern(Str(), 
     Map({
         'type': Str(), 
         'id': Str(), 
-        Optional('connections'): MapPattern(Str(), 
-                                            Enum(['CONTAINS', 'CONTROLS', 'FEEDS', 'HAS_PART', 'HAS_RANGE'])),
+        Optional('connections'): Seq(Str())
+                                   | MapPattern(Str(), 
+                                                Enum(['CONTAINS', 'CONTROLS', 'FEEDS', 'HAS_PART', 'HAS_RANGE'])),
         Optional('links'): MapPattern(Str(), 
             MapPattern(Str(), Str())),
         Optional('translation'): Any(),
@@ -19,6 +35,8 @@ schema = MapPattern(Str(),
 
 translation_schema = Regex('COMPLIANT') | Any()
 
+# TODO add manual check for translation_data_schema to de-duplicate units/unit_values/states
+# TODO add all units/unit_values/states to translation_data_schema
 translation_data_schema = Str() | Map({
                                     'present_value': Str(),
                                     Optional('states'): Map({
@@ -57,7 +75,7 @@ def load_yaml_with_schema(filepath, schema):
         parsed = load(f, schema)
         return parsed
     except YAMLValidationError as error:
-        print(error)
+        raise error
 
 def main(filename):
     yaml = load_yaml_with_schema(filename, schema)
@@ -81,7 +99,7 @@ if __name__ == '__main__':
     if len(sys.argv) != 2:
         print('USAGE: python3 schema.py yaml_file_path')
         sys.exit(1)
-    
+
     filename = sys.argv[1]
     yaml = main(filename)
 
