305e47da00da3055b9cc00c10a29583a1987c6e4
Raymond Li <li.raymond@columbia.edu>
2020-07-21 11:28:04 -0400

    Modify folder_yaml_regex filepath

--- tools/validators/ontology_validator/yamlformat/validator/config_folder_lib.py
+++ tools/validators/ontology_validator/yamlformat/validator/config_folder_lib.py
@@ -62,7 +62,7 @@ class ConfigFolder(findings_lib.Findings):
     self._component_type = component_type
 
     self._folderpath = folderpath
-    self._this_folder_yaml_regex = re.compile(r'^{0}/.*\.yaml'.format(
+    self._this_folder_yaml_regex = re.compile(r'^{0}(/|\\).*\.yaml'.format(
         self._folderpath))
 
     self._namespace_name = self._GetNamespaceFromPath()
@@ -120,7 +120,7 @@ class ConfigFolder(findings_lib.Findings):
     if not self._IsYamlUnderThisFolder(config_filename):
       self.AddFinding(
           findings_lib.InconsistentFileLocationError(
-              self._folderpath + r'\\*.yaml|/*.yaml', context))
+              self._folderpath + r'/*.yaml', context))
       return
 
     for document in documents:
