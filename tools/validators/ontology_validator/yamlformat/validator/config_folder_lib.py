d5ed48659e4860d23277d729a20687bf750f202a
Raymond Li <li.raymond@columbia.edu>
2020-07-22 15:38:00 -0400

    Add another forward slash to escape

--- tools/validators/ontology_validator/yamlformat/validator/config_folder_lib.py
+++ tools/validators/ontology_validator/yamlformat/validator/config_folder_lib.py
@@ -30,7 +30,7 @@ def IsValidFolderForType(path, component_type):
   Returns:
     True if the path is valid.
   """
-  m = re.match(r'(\w*)[/\]?{0}'.format(base_lib.SUBFOLDER_NAMES[component_type]),
+  m = re.match(r'(\w*)[/\\]?{0}'.format(base_lib.SUBFOLDER_NAMES[component_type]),
                path)
   if m is None:
     return False
